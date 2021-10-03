f7336e899dcd58e910ad57bfa5d5adb87289f43d
afraser502 <afraser502@hotmail.com>
2021-10-03 12:55:04 +0800

    Week 1 assignment

---
+++ week01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	i, j, k := "Go", 42, true
+
+	fmt.Println("Hello, World!")
+	fmt.Printf("Printing, %T!\n", i)
+	fmt.Printf("Printing, %T!\n", j)
+	fmt.Printf("Printing, %T!\n", k)
+
+}
